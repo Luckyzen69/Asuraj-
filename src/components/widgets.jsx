1a7e7ab2ff46c9a0e3011bc7ae2603b8dece7223
Lagzen Thakuri <thakurizen2@gmail.com>
2024-03-18 23:43:49 +0545

    Update feed and widgets components

--- src/components/widgets.jsx
+++ src/components/widgets.jsx
@@ -3,11 +3,42 @@
 export default function (){
     return (
         <>
-        <div className="border p-1 sticky top-0">
+        <div className="border p-1 m-2 sticky top-0 ">
 
-        <h2 className="font-bold">What's new</h2>
+        <h2 className="font-bold">Excited or Depressed</h2>
+        <div className="space-y-5">
+            <h2 className="font-bold font-mono">#Football</h2>
+            <div>
+                <p>Man City vs Real Madrid</p>
+                <p>Date: April 11, 2024</p>
+            </div>
+            <hr />
+            <div>
+                <p>Barca vs Psg</p>
+                <p>Date: April 11, 2024</p>
+            </div>
+            <hr />
+            <div>
+                <p>Bayern vs Arsenal</p>
+                <p>Date: April 11, 2024</p>
+            </div>
+            <hr />
+            <div>
+                <p>Atletico vs BVB  </p>
+                <p>Date: April 11, 2024</p>
+            </div>
+            <hr />
+        <h2 className="font-bold font-mono">#Music</h2>
         <div>
-            <h2>Football</h2>
+                <p> Radiohead</p>
+                <p>Song: No suprises</p>
+            </div>
+            <hr />
+        <div>
+                <p> Lord Huron</p>
+                <p>Song: The night we met</p>
+            </div>
+            <hr />
         </div>
         </div>
         </>
